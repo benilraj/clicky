8acf12e1a302b82572e0143f036e4b33bcb81ebf
benilraj <33128779+benilraj@users.noreply.github.com>
2025-02-21 16:26:53 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,21 @@
-const CACHE_NAME = "random-blue-circle-v1";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/manifest.json",
-    "/service-worker.js",
-    "https://unpkg.com/vue@3/dist/vue.global.js"
-];
-
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
+        caches.open("pwa-cache").then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/manifest.json",
+                "/icon.png",
+                "/icon-512.png"
+            ]);
         })
     );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames
-                    .filter((cache) => cache !== CACHE_NAME)
-                    .map((cache) => caches.delete(cache))
-            );
         })
     );
 });
