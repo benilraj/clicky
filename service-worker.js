760601c865c4bed4e62f8eaebfdb3a6481accb2c
benilraj <33128779+benilraj@users.noreply.github.com>
2025-02-21 16:09:56 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "random-blue-circle-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/service-worker.js",
+    "https://unpkg.com/vue@3/dist/vue.global.js"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cache) => cache !== CACHE_NAME)
+                    .map((cache) => caches.delete(cache))
+            );
+        })
+    );
+});
